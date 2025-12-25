8773511ba27605680d1f9ce1bfd1e32deb707e7a
MithilaKhan <mithilakhan082@gmail.com>
2025-12-25 12:54:47 +0600

    update video

--- src/feature/home/components/WhoWeAre.tsx
+++ src/feature/home/components/WhoWeAre.tsx
@@ -1,65 +1,99 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { GoArrowRight } from "react-icons/go";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const WhoWeAre = () => {
+  const videoWrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!videoWrapperRef.current) return;
+
+    gsap.fromTo(
+      videoWrapperRef.current,
+      {
+        scale: 1,
+        borderRadius: "16px",
+        pin: true,
+      },
+      {
+        scale: 1.2,
+        borderRadius: "16px",
+        ease: "none",
+        scrollTrigger: {
+          trigger: videoWrapperRef.current,
+          start: "50% 50%",
+          end: "+=100%",
+          scrub: true,
+          pin: true,
+          // pinSpacing: false,
+          anticipatePin: 1,
+          markers: false,
+        },
+      }
+    );
+
+    return () => {
+      ScrollTrigger.getAll().forEach((t) => t.kill());
+    };
+  }, []);
+
   return (
-    <div className=" min-h-screen w-full bg-linear-to-b from-[#041504] to-[#09320A]">
+    <div className="min-h-screen w-full bg-linear-to-b from-[#041504] to-[#09320A]">
       <div className="container pt-20 pb-16 lg:pt-[140px] lg:pb-[73px]">
-        <div className=" flex flex-col md:flex-row items-start justify-between gap-6">
+        <div className="flex flex-col md:flex-row items-start justify-between gap-6">
           <div>
-            <p className="text-[32px] font-semibold text-primary pb-6 ">
-              {" "}
-              Sparktech{" "}
+            <p className="text-[32px] font-semibold text-primary pb-6">
+              Sparktech
             </p>
-            <p className=" section-title font-bold! text-[#FDFDFD]! ">
-              {" "}
-              Who We Are?{" "}
+            <p className="section-title font-bold text-[#FDFDFD]">
+              Who We Are?
             </p>
           </div>
 
-          <div className=" space-y-6">
-            <p className=" text-[#D1D1D1] font-normal lg:text-xl max-w-xl">
+          <div className="space-y-6">
+            <p className="text-[#D1D1D1] font-normal lg:text-xl max-w-xl">
               We are the Sparktech team: a collaborative group of engineers,
               designers, and strategists. We approach every challenge with
               empathy, united by a passion for building digital solutions that
               are intuitively human-centered, profoundly reliable, and expertly
               engineered to unlock your potential.
             </p>
-            <div className="flex items-center justify-start">
 
-              <Link href="/company" className=" flex items-center justify-center text-primary font-medium border border-primary py-4 px-6 rounded-full gap-1 hover:bg-primary hover:text-white transition-all duration-300 cursor-pointer">
-                <span> View More </span>
-                <span>
-                  <GoArrowRight size={24} />
-                </span>
-              </Link>
-            </div>
+            <Link
+              href="/company"
+              className="inline-flex items-center gap-2 text-primary font-medium border border-primary py-4 px-6 rounded-full hover:bg-primary hover:text-white transition-all"
+            >
+              View More <GoArrowRight size={22} />
+            </Link>
           </div>
         </div>
 
-        <div className="lg:h-[500px] h-fit w-full pt-10">
-          <div className="relative   rounded-2xl shadow-lg lg:h-[500px]">
-            <div className="relative rounded-2xl overflow-hidden bg-black aspect-video lg:h-[500px] h-fit backdrop-blur-md w-full border border-transparent p-1.5">
-              {/* Video background */}
-              <div className="absolute inset-0">
-                <video
-                  className="w-full h-fit lg:h-[500px] object-cover "
-                  autoPlay
-                  loop
-                  muted
-                  playsInline
-                  disablePictureInPicture
-                  draggable={false}
-                >
-                  <source
-                    src="https://res.cloudinary.com/dnallbxxa/video/upload/v1766578516/spark-video_1_phuyfz.mp4"
-                    type="video/mp4"
-                  />
-                  Your browser does not support the video tag.
-                </video>
-              </div>
-            </div>
+        {/* VIDEO SECTION */}
+        <div className=" w-full py-10">
+          <div
+            ref={videoWrapperRef}
+            className="relative w-full h-full overflow-hidden bg-black rounded-2xl"
+          >
+            <video
+              className="w-full h-full object-cover"
+              autoPlay
+              loop
+              muted
+              playsInline
+              disablePictureInPicture
+              draggable={false}
+            >
+              <source
+                src="https://res.cloudinary.com/dnallbxxa/video/upload/v1766578516/spark-video_1_phuyfz.mp4"
+                type="video/mp4"
+              />
+            </video>
           </div>
         </div>
       </div>
